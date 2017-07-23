787dff65da51a7c9825526c9554f0c78be2f693c
srikanth <8de278+cbmigdmottlsg@sharklasers.com>
2017-07-23 13:39:53 -0700

    added a program on addition

---
+++ Test/src/com/practice/git/Addition.java
@@ -0,0 +1,14 @@
+package com.practice.git;
+
+public class Addition {
+	
+	public static int add(int a,int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println(Addition.add(3, 5));
+	}
+
+}
